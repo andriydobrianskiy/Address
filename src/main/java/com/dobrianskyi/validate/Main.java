cdf422c9e9fb43f8630f456e1e6e34a03637e931
andriydobrianskiy <andriydobrianskiy@ukr.net>
2020-05-29 09:53:38 +0300

    second

--- src/main/java/com/dobrianskyi/validate/Main.java
+++ src/main/java/com/dobrianskyi/validate/Main.java
@@ -1,38 +1,42 @@
 package com.dobrianskyi.validate;
 
 import java.io.BufferedReader;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 
 public class Main {
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             System.out.println("Enter the sentence:");
-           String value = reader.readLine();
+            String value = reader.readLine();
             System.out.println("Enter the regex:");
             String regex = reader.readLine();
             System.out.println("Enter the replace:");
             String replace = reader.readLine();
-            String output= value.replace(regex, replace);
+            String output = value.replace(regex, replace);
             System.out.println(output);
 
-            System.out.println(value.indexOf(regex));
-
-
-            Pattern pattern = Pattern.compile(regex);
-            Matcher matcher = pattern.matcher(value);
-            while (matcher.find()) {
-                System.out.println(value.substring(matcher.start(), matcher.end()));
-            }
+            System.out.println(change(value, regex, replace));
 
 
         } catch (IOException | NumberFormatException e) {
             System.out.println("Enter the number");
         }
 
+    }
+
+    static String change(String str, String regex, String replace) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(str);
+        int i = 0;
+        while ((i = sb.indexOf(regex, i)) >= 0) {
+            sb.delete(i, i + regex.length());
+            sb.insert(i, replace);
+            i += replace.length();
+        }
+        return sb.toString();
     }
 
 }
